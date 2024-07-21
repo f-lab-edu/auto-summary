ee0fc9717134a89090f264135699d5519d8b8ae3
sksowk156 <sksowk156@gmail.com>
2024-07-22 04:38:19 +0900

    [Fix] clean project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.android.application)
     alias(libs.plugins.klint)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)

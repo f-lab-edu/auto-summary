1a3cad0873e1e4b50ebd57fc18c2d42b2ae0dd4e
sksowk156 <sksowk156@gmail.com>
2024-07-23 00:38:55 +0900

    [Fix] buildscript classpath 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.hilt.android.gradle.plugin)
+        classpath(libs.kotlin.serialization)
+    }
+}
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false

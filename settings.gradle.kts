08fa4e55126e5ade491e86a97c904a5e4fdc6ca5
sksowk156 <sksowk156@gmail.com>
2024-07-23 00:07:00 +0900

    [Fix] plugins 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.4.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)

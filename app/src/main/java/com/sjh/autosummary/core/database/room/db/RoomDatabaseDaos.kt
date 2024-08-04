983e3fe5a1d5048c224561d4c2d6b48aaff40ad0
jerry@f-lab.kr <jerry@f-lab.kr>
2024-08-04 15:45:53 +0900

    Code style

--- app/src/main/java/com/sjh/autosummary/core/database/room/db/RoomDatabaseDaos.kt
+++ app/src/main/java/com/sjh/autosummary/core/database/room/db/RoomDatabaseDaos.kt
@@ -10,73 +10,71 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class RoomDatabaseDaos
-    @Inject
-    constructor(
-        private val chatHistoryDao: ChatHistoryDao,
-        private val messageContentDao: MessageContentDao,
-    ) : LocalDataSource {
-        override suspend fun getChatHistoryWithMessagesById(id: Long): Result<ChatHistoryWithMessages?> {
-            return withContext(Dispatchers.IO) {
-                try {
-                    val chatHistory = chatHistoryDao.getChatHistoryById(id)
-                    val messageContents =
-                        chatHistory?.let { messageContentDao.getMessageContentsByChatHistoryId(it.id) }
-                    if (chatHistory != null && messageContents != null) {
-                        Result.success(ChatHistoryWithMessages(chatHistory, messageContents))
-                    } else {
-                        Result.success(null)
-                    }
-                } catch (e: Exception) {
-                    Result.failure(e)
+class RoomDatabaseDaos @Inject constructor(
+    private val chatHistoryDao: ChatHistoryDao,
+    private val messageContentDao: MessageContentDao,
+) : LocalDataSource {
+    override suspend fun getChatHistoryWithMessagesById(id: Long): Result<ChatHistoryWithMessages?> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val chatHistory = chatHistoryDao.getChatHistoryById(id)
+                val messageContents =
+                    chatHistory?.let { messageContentDao.getMessageContentsByChatHistoryId(it.id) }
+                if (chatHistory != null && messageContents != null) {
+                    Result.success(ChatHistoryWithMessages(chatHistory, messageContents))
+                } else {
+                    Result.success(null)
                 }
-            }
-        }
-
-        override suspend fun getAllChatHistoriesWithMessages(): Result<List<ChatHistoryWithMessages>> {
-            return withContext(Dispatchers.IO) {
-                try {
-                    val chatHistories = chatHistoryDao.getAllChatHistories()
-                    val chatHistoriesWithMessages =
-                        chatHistories.map { chatHistory ->
-                            val messageContents =
-                                messageContentDao.getMessageContentsByChatHistoryId(chatHistory.id)
-                            ChatHistoryWithMessages(chatHistory, messageContents)
-                        }
-                    Result.success(chatHistoriesWithMessages)
-                } catch (e: Exception) {
-                    Result.failure(e)
-                }
-            }
-        }
-
-        override suspend fun insertMessageContent(messageContent: MessageContentEntity) {
-            withContext(Dispatchers.IO) {
-                messageContentDao.insertMessageContent(messageContent)
-            }
-        }
-
-        override suspend fun insertChatHistoryWithMessages(
-            chatHistoryEntity: ChatHistoryEntity,
-            messageContentEntitys: List<MessageContentEntity>,
-        ): Result<Long> {
-            return withContext(Dispatchers.IO) {
-                try {
-                    val chatHistoryId = chatHistoryDao.insertChatHistory(chatHistoryEntity)
-                    messageContentEntitys.forEach { messageContent ->
-                        messageContentDao.insertMessageContent(messageContent.copy(chatHistoryId = chatHistoryId))
-                    }
-
-                    Result.success(chatHistoryId)
-                } catch (e: Exception) {
-                    Result.failure(e)
-                }
-            }
-        }
-
-        override suspend fun deleteChatHistoryById(chatHistoryId: Long) {
-            withContext(Dispatchers.IO) {
-                chatHistoryDao.deleteChatHistoryById(chatHistoryId)
+            } catch (e: Exception) {
+                Result.failure(e)
             }
         }
     }
+
+    override suspend fun getAllChatHistoriesWithMessages(): Result<List<ChatHistoryWithMessages>> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val chatHistories = chatHistoryDao.getAllChatHistories()
+                val chatHistoriesWithMessages =
+                    chatHistories.map { chatHistory ->
+                        val messageContents =
+                            messageContentDao.getMessageContentsByChatHistoryId(chatHistory.id)
+                        ChatHistoryWithMessages(chatHistory, messageContents)
+                    }
+                Result.success(chatHistoriesWithMessages)
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+    }
+
+    override suspend fun insertMessageContent(messageContent: MessageContentEntity) {
+        withContext(Dispatchers.IO) {
+            messageContentDao.insertMessageContent(messageContent)
+        }
+    }
+
+    override suspend fun insertChatHistoryWithMessages(
+        chatHistoryEntity: ChatHistoryEntity,
+        messageContentEntitys: List<MessageContentEntity>,
+    ): Result<Long> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val chatHistoryId = chatHistoryDao.insertChatHistory(chatHistoryEntity)
+                messageContentEntitys.forEach { messageContent ->
+                    messageContentDao.insertMessageContent(messageContent.copy(chatHistoryId = chatHistoryId))
+                }
+
+                Result.success(chatHistoryId)
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+    }
+
+    override suspend fun deleteChatHistoryById(chatHistoryId: Long) {
+        withContext(Dispatchers.IO) {
+            chatHistoryDao.deleteChatHistoryById(chatHistoryId)
+        }
+    }
+}

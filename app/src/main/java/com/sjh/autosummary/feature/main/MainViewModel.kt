d80b1e160f324b3a02e3b617a8c62ecf478b2e96
sksowk156 <sksowk156@gmail.com>
2024-08-04 20:04:16 +0900

    [Fix] ktlint 적용

--- app/src/main/java/com/sjh/autosummary/feature/main/MainViewModel.kt
+++ app/src/main/java/com/sjh/autosummary/feature/main/MainViewModel.kt
@@ -1,20 +1,9 @@
 package com.sjh.autosummary.feature.main
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.sjh.autosummary.core.common.LoadState
-import com.sjh.autosummary.core.data.model.ChatRequest
 import com.sjh.autosummary.core.data.repository.ChatRepository
 import com.sjh.autosummary.core.data.repository.HistoryRepository
-import com.sjh.autosummary.core.model.ChatHistory
-import com.sjh.autosummary.core.model.MessageContent
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.cancellable
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel

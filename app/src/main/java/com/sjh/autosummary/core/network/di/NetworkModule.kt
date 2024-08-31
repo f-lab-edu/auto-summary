bb7650704e8a663d094f93505d5aa535678458c8
sksowk156 <sksowk156@gmail.com>
2024-09-01 01:44:18 +0900

    [Fix] ktlint 적용

--- app/src/main/java/com/sjh/autosummary/core/network/di/NetworkModule.kt
+++ app/src/main/java/com/sjh/autosummary/core/network/di/NetworkModule.kt
@@ -4,7 +4,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.serialization.json.Json
 import okhttp3.Call
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
